bfc0177452952ea5d5bc4747f185dbd2c2b42e08
Geony Ayyad <geonyayyad@Geonys-MBP.fios-router.home>
2018-04-07 11:24:27 -0400

    new changes with new packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { pushRotate as Menu } from 'react-burger-menu';
+import Logo from './components/Logo'
 import './App.css';
+import { Well, Button, Jumbotron, Col, SplitButton, MenuItem, Glyphicon, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
+import { slideInLeft } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+
+const styles = {
+  slideInLeft: {
+    animation: 'x 3s',
+    animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+  }
+}
+ 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      
+      <div id="outer-container">
+  
+          <Menu 
+            left
+            pageWrapId={ "page-wrap" } 
+            outerContainerId={ "outer-container" } >
+              <a id="home" className="menu-item" href="/">Home</a>
+              <a id="about" className="menu-item" href="/about">About</a>
+              <a id="contact" className="menu-item" href="/contact">Contact</a>
+              <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+          </Menu>
+
+  <main id="page-wrap">
+    <div className="App-header">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Logo />
+      <br />
+    </div>
+    
+    <div className="Introduction-section">
+      <div className="container">
+      <StyleRoot>
+      <div className="test" style={styles.slideInLeft}>
+      <h1> Hi!</h1>
       </div>
+    </StyleRoot>
+      </div>
+    </div>
+  </main>
+      </div>
+
+    
+      
+
     );
   }
 }
---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const style = {
+  width: "700px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  display: "block"
+
+}
+
+const Logo = props =>
+  <div>
+    <img style={style} src="gasoftwaredeveloper.png" />
+  </div>;
+
+export default Logo;
